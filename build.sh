287e99152f51088abd7f6c9de519b89639410be0
Soaicl-as <social.marketing638@gmail.com>
2025-04-04 16:34:30 -0700

    build.sh

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt-get update
+apt-get install -y \
+    libglib2.0-0 \
+    libnss3 \
+    libx11-6 \
+    libxcb1 \
+    libxcomposite1 \
+    libxcursor1 \
+    libxdamage1 \
+    libxext6 \
+    libxfixes3 \
+    libxi6 \
+    libxrandr2 \
+    libxrender1 \
+    libxtst6
+
+pip install -r requirements.txt
